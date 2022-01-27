ebdbcf2ffa7fb7564e41a80ac5f9c09358f9f159
binh.buidang <dangbinh4869@gmail.com>
2022-01-28 03:12:19 +0700

    add: book model.

---
+++ models.ts
@@ -0,0 +1,20 @@
+export interface Book {
+  Id: string;
+  Name: string;
+  RatingDist1: string;
+  pagesNumber: number;
+  RatingDist4: string;
+  RatingDistTotal: string;
+  PublishMonth: number;
+  PublishDay: number;
+  Publisher: string;
+  CountsOfReview: number;
+  PublishYear: number;
+  Language: string;
+  Authors: string;
+  Rating: number;
+  RatingDist2: string;
+  RatingDist5: string;
+  ISBN?: any;
+  RatingDist3: string;
+}
